fb295c41a6d6f6017d44f98b0b95f4d2d771b24e
Alan Mathew <151936802+alanmathew23@users.noreply.github.com>
2024-05-19 19:35:18 +0530

    Delete input_data.h

--- input_data.h
+++
@@ -1,7 +0,0 @@
-
-#define STUDENT_DATA_H
-
-void Data_Input(char name[],float isa1m[],float isa2m[]);
-float Calc(float marks[]);
-char Grade(float percentage);
-void GradeCard(char name[], char grade1, char grade2); 
